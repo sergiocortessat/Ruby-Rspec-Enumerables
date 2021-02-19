ba18ae0e1128304137183b620abb90d6f4c113f6
Billo Diallo <billodiallone@gmail.com>
2021-02-19 11:31:13 +0300

    adding enumerable file

--- README.md
+++ README.md
@@ -1,87 +1,57 @@
-![](https://img.shields.io/badge/Microverse-blueviolet)
+# Enumerables
 
-# Week 2 TicTacToe Rspec
+## Table of Contents
 
-In order to win the game, a player must place three of their marks in a horizontal, vertical, or diagonal row.
-Players will soon discover that the best play from both parties leads to a draw.
-Hence, tic-tac-toe is most often played by young children, who often have not yet discovered the optimal strategy. 
+* [About the Project](#about-the-project)
+  * [Built With](#built-with)
+* [Contributing](#contributing)
+* [Contact](#contact)
+* [Acknowledgements](#acknowledgements)
 
-## How do we play Tic Tac Toe?
+<!-- ABOUT THE PROJECT   -->
+## About The Project
+In this project, we develop the already existing Ruby enumerable methods such as #each and many others, using our existing knowledge of important concepts in Ruby. 
 
-Players take turns playing: 
-- 1st turn — Player 1: 
-Player 1 places their token (X) in only 1 square of their choice
-- 2nd turn — Player 2:
- Player 2 places their token (O) in only 1 square of their choice, except they can’t touch the square that Player 1’s token already fills
-- 3rd turn- Player 1 …. and the cycle continues until a player wins or there’s a tie game
+### Built With
 
-There are 8 winning combinations: Top row, Middle row, Bottom row, Left column, Middle column, Right column, Left diagonal, and Right diagonal
-There’s a draw/tie when the entire board is filled with X’s and O’s but no winning combinations can be found.
+* [RUBY](https://github.com/billodiallo/project2-enumerable/tree/feature-enum)
 
-## Screenshot
-- testing
+## Contributing
 
-![rspectest-tictoc-toe](https://user-images.githubusercontent.com/11162987/108459668-2cf05a00-7288-11eb-9c1f-61079e7ce200.PNG)
-
-![Screenshot](TicTacToe.gif)
-
-## Built With
- [RUBY](https://github.com/billodiallo/Rspec-tic_tic_toe)
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
 
 ## Getting Started
-
 This is an example of how you may give instructions on setting up your project locally. Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.
 
 - To get a local copy up and running follow these simple example steps.
 
-- Clone this repository with git clone ```https://github.com/billodiallo/Rspec-tic_tic_toe.git``` using your terminal or command line.
+- Clone this repository with git clone ```https://github.com/billodiallo/project2-enumerable.git``` using your terminal or command line.
 - Change to the project directory by entering :
-```cd Rspec-tic_tic_toe``` in the terminal .
-- run the command ```bin/main```
-
-## Prequisites
-
-Ruby installed on your local machine.
-Terminal or similar to execute the program.
-
-## Testing
-In this project the testing was done using RSpec to test all game logic methods.
-
-## Install
-- In a terminal window type gem install rspec
-- Once rspec install has finished, type rspec --init
-- You will see a folder spec and a file .rspec
-- Inside spec folder you'll see a spec_helper.rb file.
-
-## Run
-We have already created rspec-testing files called logic.rb, and settings.rb.
-- go to the folder inside your terminal type rspec
-- If all test pass you will see:
- `16 examples, 0 failures` for logic.rb
- `4 examples, 0 failures` for settings.rb
+```cd project2-enumerable``` in the terminal .
+- run the command ```ruby project2-enumerable```
+<!-- CONTACT -->
+## Contact
 
 ## Authors
 
-👤 Billo Dallio
+👤 **Billo Diallo**
 
-- GitHub: [@billodiallo](https://github.com/billodiallo)
+- GitHub:[@billodiallo](https://github.com/billodiallo)
 - Twitter: [@BilloDi83547008](https://twitter.com/BilloDi83547008)
+- Linkedin:[@mabillodiallo](https://www.linkedin.com/in/mabillodiallo/)
 
-👤 Sergio Cortes Satizabal
 
-- Github: [@sergiocortessat](https://github.com/sergiocortessat)
-- Twitter: [@sergiocortessat](https://twitter.com/sergiocortessat)
+👤 **Elvis Kariithi Githinji**
 
-## 🤝 Contributing
+- GitHub:[@karvel-code](https://github.com/karvel-code)
+- Linkedin:[@elvis-githinji-9a5032164](https://www.linkedin.com/in/elvis-githinji-9a5032164/)
 
-Contributions, issues, and feature requests are welcome!
+Project Link: [Bubble Sort Ruby](https://github.com/billodiallo/bubble_sort/tree/feature-b)
 
-Feel free to check the [issues page](https://github.com/greg0109/TicTacToe/issues).
 
-## Show your support
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements
 
-Give a ⭐️ if you like this project!
+* [Microverse](https://www.microverse.org/)
 
-## 📝 License
 
-This project is [MIT](LICENSE) licensed.
---
+++ enumerable.rb
@@ -0,0 +1,190 @@
+module Enumerable
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    i = 0
+    while i < to_a.length
+      yield to_a[i]
+      i += 1
+    end
+    self
+  end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    i = 0
+    while i < to_a.length
+      yield(to_a[i], i)
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    new_arr = []
+    my_each { |item| new_arr << item if yield item }
+    new_arr
+  end
+
+  def my_all?(param = nil)
+    if block_given?
+      to_a.my_each { |item| return false if yield(item) == false }
+      return true
+    elsif param.nil?
+      to_a.my_each { |item| return false if item.nil? || item == false }
+    elsif !param.nil? && (param.is_a? Class)
+      to_a.my_each { |item| return false unless [item.class, item.class.superclass].include?(param) }
+    elsif !param.nil? && param.instance_of?(Regexp)
+      to_a.my_each { |item| return false unless param.match(item) }
+    else
+      to_a.my_each { |item| return false if item != param }
+    end
+    true
+  end
+
+  def my_any?(param = nil)
+    if block_given?
+      to_a.my_each { |item| return true if yield(item) }
+      return false
+    elsif param.nil?
+      to_a.my_each { |item| return true if item }
+    elsif !param.nil? && (param.is_a? Class)
+      to_a.my_each { |item| return true if [item.class, item.class.superclass].include?(param) }
+    elsif !param.nil? && param.instance_of?(Regexp)
+      to_a.my_each { |item| return true if param.match(item) }
+    else
+      to_a.my_each { |item| return true if item == param }
+    end
+    false
+  end
+
+  def my_none?(arg = nil)
+    if !block_given? && arg.nil?
+      my_each { |n| return false if n }
+      return true
+    end
+
+    if !block_given? && !arg.nil?
+
+      if arg.is_a?(Class)
+        my_each { |n| return false if n.instance_of?(arg) }
+        return true
+      end
+
+      if arg.instance_of?(Regexp)
+        my_each { |n| return false if arg.match(n) }
+        return true
+      end
+
+      my_each { |n| return false if n == arg }
+      return true
+    end
+    my_any? { |item| return false if yield(item) }
+    true
+  end
+
+  def my_count(param = nil)
+    c = 0
+    if block_given?
+      to_a.my_each { |item| c += 1 if yield(item) }
+    elsif !block_given? && param.nil?
+      c = to_a.length
+    else
+      c = to_a.my_select { |item| item == param }.length
+    end
+    c
+  end
+
+  def my_map(proc_x = nil)
+    return enum_for unless block_given?
+
+    map_list = []
+    if proc_x.nil?
+      my_each { |element| map_list.push(yield(element)) }
+    else
+      my_each { |element| map_list.push(proc_x.call(element)) }
+    end
+    map_list
+  end
+
+  def my_inject(initial = nil, sym = nil)
+    if (!initial.nil? && sym.nil?) && (initial.is_a?(Symbol) || initial.is_a?(String))
+      sym = initial
+      initial = nil
+    end
+    if !block_given? && !sym.nil?
+      to_a.my_each { |item| initial = initial.nil? ? item : initial.send(sym, item) }
+    else
+      to_a.my_each { |item| initial = initial.nil? ? item : yield(initial, item) }
+    end
+    initial
+  end
+end
+
+def multiply_els(par)
+  par.my_inject(1) { |a, b| a * b }
+end
+
+
+puts '1.--------my_each--------'
+%w[Sharon Leo Leila Brian Arun].my_each { |friend| puts friend }
+
+puts '2.--------my_each_with_index--------'
+%w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts friend if index.even? }
+
+puts '3.--------my_select--------'
+puts (%w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' })
+
+puts '4.--------my_all--------'
+puts (%w[ant bear cat].my_all? { |word| word.length >= 3 }) #=> true
+puts (%w[ant bear cat].my_all? { |word| word.length >= 4 }) #=> false
+puts %w[ant bear cat].my_all?(/t/) #=> false
+puts [1, 2i, 3.14].my_all?(Numeric) #=> true
+puts [].my_all? #=> true
+
+puts '5.--------my_any--------'
+puts (%w[ant bear cat].my_any? { |word| word.length >= 3 }) #=> true
+puts (%w[ant bear cat].my_any? { |word| word.length >= 4 }) #=> true
+puts %w[ant bear cat].my_any?(/d/) #=> false
+puts [nil, true, 99].my_any?(Integer) #=> true
+puts [nil, true, 99].my_any? #=> true
+puts [].my_any? #=> false
+
+puts '6.--------my_none--------'
+puts (%w[ant bear cat].my_none? { |word| word.length == 5 }) #=> true
+puts (%w[ant bear cat].my_none? { |word| word.length >= 4 }) #=> false
+puts %w[ant bear cat].my_none?(/d/) #=> true
+puts [1, 3.14, 42].my_none?(Float) #=> false
+puts [].my_none? #=> true
+puts [nil].my_none? #=> true
+puts [nil, false].my_none? #=> true
+puts [nil, false, true].my_none? #=> false
+
+puts '7.--------my_count--------'
+arr = [1, 2, 4, 2]
+puts arr.my_count #=> 4
+puts arr.my_count(2) #=> 2
+puts (arr.my_count { |x| (x % 2).zero? }) #=> 3
+
+puts '8.--------my_maps--------'
+my_order = ['medium Big Mac', 'medium fries', 'medium milkshake']
+puts (my_order.my_map { |item| item.gsub('medium', 'extra large') })
+puts ((0..5).my_map { |i| i * i })
+puts 'my_map_proc'
+my_proc = proc { |i| i * i }
+puts (1..5).my_map(my_proc) { |i| i + i }
+
+puts '8.--------my_inject--------'
+puts ((1..5).my_inject { |sum, n| sum + n }) #=> 15
+puts (1..5).my_inject(1) { |product, n| product * n } #=> 120
+longest = %w[ant bear cat].my_inject do |memo, word|
+  memo.length > word.length ? memo : word
+end
+puts longest #=> "bear"
+
+puts 'multiply_els'
+puts multiply_els([2, 4, 5]) #=> 40
+
